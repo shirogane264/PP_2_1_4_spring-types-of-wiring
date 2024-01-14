ce9cb356284f9a893e9a950e2772c1971619c796
Кирилл <kasss614@gmail.com>
2024-01-14 21:42:21 +0500

    done

--- src/main/java/koschei/models/Duck5.java
+++ src/main/java/koschei/models/Duck5.java
@@ -5,9 +5,16 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Duck5 {
-    @Autowired
+
     private Egg6 egg;
 
+    @Autowired
+    public void setEgg(Egg6 egg) {
+        this.egg = egg;
+    }
+
     @Override
-    public String toString() { return ", в утке яйцо " + egg.toString(); }
+    public String toString() {
+        return ", в утке яйцо " + egg.toString();
+    }
 }
